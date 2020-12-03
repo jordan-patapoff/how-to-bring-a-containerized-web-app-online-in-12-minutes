170285c4f8a87a271b2e18ebe6148094fa43d7e6
Jordan Patapoff <42149384+jordan-patapoff@users.noreply.github.com>
2020-12-02 16:19:37 -0800

    Create server.js for pt 2

---
+++ pt-2-add-a-database/server.js
@@ -0,0 +1,62 @@
+'use strict';
+const express = require('express');
+const AWS = require('aws-sdk');
+const pg = require('pg');
+AWS.config.update({region:'us-west-2'});
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+    var secretsmanager = new AWS.SecretsManager();
+    secretsmanager.getSecretValue({ SecretId: 'MyJDPRDSInstanceSecret55555-55555' }, function(err, data) {
+      if (err) {
+        res.send('Unable to get secret');
+      }
+      else {
+        console.log("got back the secrets");
+
+        var secrets = JSON.parse(data.SecretString);
+        
+        const client = new pg.Client({
+          user: secrets.username,
+          host: secrets.host,
+          database: 'postgres',
+          password: secrets.password,
+          port: secrets.port,
+          connectionTimeoutMillis: 3000
+        });
+        
+        client.connect(err => {
+          if (err) {
+            console.error('connection error', err.stack)
+            res.send('connection error');
+          } else {
+            
+            console.log('connected')
+            
+            client.query('SELECT NOW()', (queryerr, queryres) => {
+              console.log(queryerr, queryres)
+              
+              client.end();
+              
+              if(queryerr) {
+                res.send(queryerr);
+              } else {
+                res.send(queryres.rows[0].now);
+              }
+              
+            });
+        
+          }
+        });
+        
+      }
+    });
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
